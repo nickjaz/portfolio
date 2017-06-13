df329e299573025744cb45b90cf4eb2fa694a0a0
Nick Asmussen <nick.asmussen1@gmail.com>
2017-06-13 10:00:02 -0700

    Adds scaffolding and constructor function

---
+++ app.js
@@ -0,0 +1,13 @@
+'user strict'
+
+// contructor function to add later projects
+
+function Project(name, date, webPath, collabs, tools, about, gitPath){
+  this.name = name;
+  this.date = date;
+  this.webPath = webPath;
+  this.collabs = collabs;
+  this.tools = tools;
+  this.about = about;
+  this.gitPath = gitPath;
+}

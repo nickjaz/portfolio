22982e7453906c595de51dc825ad6bd79bf3ae83
Nick Asmussen <nick.asmussen1@gmail.com>
2017-06-29 20:09:15 -0700

    add express request proxy

--- public/scripts/view/repoView.js
+++ public/scripts/view/repoView.js
@@ -5,9 +5,18 @@ var app = app || {};
 (function(module) {
   const repoView = {};
 
+  const ui = function () {
+    let $repo = $('#repo');
+
+    $repo.find('ul').empty();
+    $repo.show().siblings().hide();
+  };
+
   var render = Handlebars.compile($('#repo_template').html());
 
   repoView.index = function() {
+    ui();
+
     $('#repo ul').append(
       app.repo.with('name').map(render)
     );
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const app = express();
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('./public'));
